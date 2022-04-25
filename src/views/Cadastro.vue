cbdd6ccfc5ec08bde4a679e2a76a792ba7706a73
Theodoro01 <theodorovieira01@gmail.com>
2022-04-25 14:09:55 -0300

    router-routerLinks

---
+++ src/views/Cadastro.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <input type="text" name="email" id="email" placeholder="E-mail"><br>
+        <input type="text" name="name" id="name" placeholder="Name"><br>
+        <button>Cadastrar</button>
+    </div>
+</template>
